429fc1b800592849fef49b2bb012c5877f451a0d
Gabriel MANOLACHE <g@gmanolache.com>
2020-02-11 11:30:50 +0100

    added docstring

--- compose-demo/app.py
+++ compose-demo/app.py
@@ -6,8 +6,15 @@ redis = Redis(host='redis', port=6379, decode_responses=True)
 
 @app.route("/")
 def hello():
+    """We increment the page hits and return the body of the page"""
     redis.incr('hits')
-    return '<p style="color:red;"> Page has been viewed %s time(s).</p>' % redis.get('hits')
+
+    text = '''
+    <p style="color:red;">
+        Page has been viewed %s time(s).
+    </p>'''
+
+    return text % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
